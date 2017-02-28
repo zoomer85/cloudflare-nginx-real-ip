7f301171c7524aeed8cebf76e00eafbc208dba67
Peter Simmons <cabbiepete@gmail.com>
2017-03-01 12:16:51 +1300

    adding nginx service restart

--- cloudflare-nginx-real-ips.sh
+++ cloudflare-nginx-real-ips.sh
@@ -4,3 +4,5 @@ for i in `curl https://www.cloudflare.com/ips-v4`; do
         echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
 done
 echo "real_ip_header CF-Connecting-IP;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+
+service nginx configtest && service nginx restart
