47d751dcd75334247c85e4d8152027696deb12e0
Peter Simmons <cabbiepete@gmail.com>
2017-03-01 12:24:03 +1300

    Renaming to match non plural names

---
+++ cloudflare-nginx-real-ip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "#Cloudflare" > /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+for i in `curl https://www.cloudflare.com/ips-v4`; do
+        echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+done
+echo "real_ip_header CF-Connecting-IP;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+
+service nginx configtest && service nginx restart
