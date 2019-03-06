410e6b5644a2c142e13cfe9b623fed63482b051e
Dmitry <dimetrius@bk.ru>
2019-03-06 16:52:50 +0200

    Update cloudflare-nginx-real-ip.sh

--- cloudflare-nginx-real-ip.sh
+++ cloudflare-nginx-real-ip.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-echo "#Cloudflare" > /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
-for i in `curl https://www.cloudflare.com/ips-v4`; do
-        echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+FILE="/etc/nginx/conf.d/00_cloudflare.conf"
+DATE=$(date)
+echo "#-------------Cloudflare------------" > $FILE;
+echo "#--- ${DATE} ---" >> $FILE;
+echo "#" >> $FILE;
+for v4 in `curl https://www.cloudflare.com/ips-v4`; do
+        echo "set_real_ip_from $v4;" >> $FILE;
 done
-echo "real_ip_header CF-Connecting-IP;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+echo "#" >> $FILE;
+for v6 in `curl https://www.cloudflare.com/ips-v6`; do
+        echo "set_real_ip_from $v6;" >> $FILE;
+done
+echo "#" >>$FILE;
+echo "real_ip_header CF-Connecting-IP;" >> $FILE;
+echo "#" >> $FILE;
 
 service nginx configtest && service nginx restart
